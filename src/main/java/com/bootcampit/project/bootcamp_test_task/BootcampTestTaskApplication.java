f4f20d9dfa6456bf4adea185a739c069ec41139a
dzmitrybetska <d.betska@gmail.com>
2024-01-20 16:16:08 +0100

    creating a project

---
+++ src/main/java/com/bootcampit/project/bootcamp_test_task/BootcampTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.bootcampit.project.bootcamp_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampTestTaskApplication.class, args);
+	}
+
+}
